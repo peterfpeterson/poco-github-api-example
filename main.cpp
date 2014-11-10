1ce13a7f58082cec3c366ea83beb0ac2ba226945
Pete Peterson <petersonpf@ornl.gov>
2014-11-10 15:02:20 -0500

    clang-format

--- main.cpp
+++ main.cpp
@@ -9,26 +9,26 @@ using namespace Poco::Net;
 
 int main(int argc, const char *argv[]) {
 
-try{
+  try {
     Poco::Net::initializeSSL();
-    Poco::SharedPtr<Poco::Net::InvalidCertificateHandler> ptrHandler = new AcceptCertificateHandler(false);
-    Context::Ptr ptrContext = new Context(Context::CLIENT_USE, "", "", "", Context::VERIFY_RELAXED, 9, true, "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH");
+    Poco::SharedPtr<Poco::Net::InvalidCertificateHandler> ptrHandler =
+        new AcceptCertificateHandler(false);
+    Context::Ptr ptrContext =
+        new Context(Context::CLIENT_USE, "", "", "", Context::VERIFY_RELAXED, 9,
+                    true, "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH");
     SSLManager::instance().initializeClient(0, ptrHandler, ptrContext);
 
     Poco::Net::SocketAddress address("www.server.com:443");
     Poco::Net::SecureStreamSocket socket(address);
-    if (socket.havePeerCertificate())
-    {
-        X509Certificate cert = socket.peerCertificate();
-        std::cout<<cert.issuerName()<<"\n"; 
+    if (socket.havePeerCertificate()) {
+      X509Certificate cert = socket.peerCertificate();
+      std::cout << cert.issuerName() << "\n";
+    } else {
+      std::cout << "No certificate";
     }
-    else
-    {
-        std::cout<<"No certificate";
-    }
-
-}catch (Poco::Exception& e) {
+  }
+  catch (Poco::Exception &e) {
     std::cout << "Error: " << e.displayText() << "\n";
     return -1;
-}
+  }
 }
